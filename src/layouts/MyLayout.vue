a8c7d31fc74295b062000a9d9fcba1f51a597086
Bardiya Bakhshandeh <sc16a3b@leeds.ac.uk>
2019-04-08 00:58:00 +0100

    added treasury page, modifications to index

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -1,6 +1,6 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-layout-header>
+  <q-layout view="lhh Lpr lff">
+    <q-layout-header reveal>
       <q-toolbar
         color="primary"
         :glossy="$q.theme === 'mat'"
@@ -32,26 +32,26 @@
         link
         inset-delimiter
       >
-        <q-list-header>Essential Links</q-list-header>
+        <q-list-header>Bardiya Bakhshandeh</q-list-header>
         <q-item @click.native="openURL('http://quasar-framework.org')">
-          <q-item-side icon="school" />
-          <q-item-main label="Docs" sublabel="quasar-framework.org" />
+          <q-item-side icon="person" />
+          <q-item-main label="About" sublabel="quasar-framework.org" />
         </q-item>
         <q-item @click.native="openURL('https://github.com/quasarframework/')">
           <q-item-side icon="code" />
-          <q-item-main label="GitHub" sublabel="github.com/quasarframework" />
+          <q-item-main label="Skills" sublabel="github.com/quasarframework" />
         </q-item>
         <q-item @click.native="openURL('https://discord.gg/5TDhbDg')">
-          <q-item-side icon="chat" />
-          <q-item-main label="Discord Chat Channel" sublabel="https://discord.gg/5TDhbDg" />
+          <q-item-side icon="school" />
+          <q-item-main label="Education" sublabel="https://discord.gg/5TDhbDg" />
         </q-item>
         <q-item @click.native="openURL('http://forum.quasar-framework.org')">
-          <q-item-side icon="record_voice_over" />
-          <q-item-main label="Forum" sublabel="forum.quasar-framework.org" />
+          <q-item-side icon="money" />
+          <q-item-main label="Treasury" sublabel="forum.quasar-framework.org" />
         </q-item>
         <q-item @click.native="openURL('https://twitter.com/quasarframework')">
-          <q-item-side icon="rss feed" />
-          <q-item-main label="Twitter" sublabel="@quasarframework" />
+          <q-item-side icon="mail" />
+          <q-item-main label="Contact" sublabel="@quasarframework" />
         </q-item>
       </q-list>
     </q-layout-drawer>
---
+++ src/pages/Treasury.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+    <h4>You are in the Treasury!</h4>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Treasury'
+}
+</script>
+
+<style>
+</style>
